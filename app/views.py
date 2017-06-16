d319fa896663f39e2614bbd15ea320eac854c0a8
André Leão <andre.sp.leao@gmail.com>
2017-06-16 13:56:23 -0300

    Lista de usernames precisava de utilizar a funcao json.loads().
    Essa funcao decodifica o objeto json enviado. Sem ela, era identificado
    cada caracter de um elemento em um for. Exemplo: se a lista estava ["teste123"]
    quando fosse feito um for, seria impresso cada caracter da palavra "teste123".

--- app/views.py
+++ app/views.py
@@ -16,22 +16,25 @@ def get_token(username,password):
 
 @csrf_exempt
 def criar_projeto(request):
-	api = TaigaAPI(token= request.POST['token'])
+	api = TaigaAPI(token=request.POST['token'])	
 	projeto = api.projects.create(request.POST['nome_projeto'], request.POST['nome_projeto'], is_private = False)
-
+	return HttpResponse('funcionou')
 @csrf_exempt
 def adicionar_colaboradores(request):
 	api = TaigaAPI(token=request.POST['token'])
-	usernames = request.POST['usernames']
+	usernames = json.loads(request.POST['usernames'])
 	projeto_nome = request.POST['nome_projeto']
 
 	projeto = api.projects.get_by_slug(projeto_nome)	
 	role = projeto.add_role('Desenvolvedor', permissions=["add_issue", "modify_issue"])
-	for username in usernames:			
-		url = 'https://api.taiga.io/api/v1/memberships'
+	
+	for username in usernames:		
+		url = 'https://api.taiga.io/api/v1/memberships'		
 		data_json ={"project": projeto.id, "role": role.id, "username": username}	
 		authorization = 'Bearer '+ api.token 	
 		headers = {'Content-Type': 'application/json', 'Authorization': authorization}
 
 		r = requests.post(url, data=json.dumps(data_json), headers=headers)
 		print(r.content)
+	return HttpResponse('funcionou')	
+
